512794a98e5f9dbdd1fbccaac3bebb98311d6e1b
Leon <tamazgan@gmail.com>
2019-05-27 15:51:55 +0300

    Completed the header menu

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,12 +4,13 @@ var gulp = require('gulp'),
     cssVars = require('postcss-simple-vars'),
     nested = require('postcss-nested'),
     cssImport = require('postcss-import'),
-    mixins = require('postcss-mixins');
+    mixins = require('postcss-mixins'),
+    hexrgba = require('postcss-hexrgba');
 
 // when 'styles' task is called:
 gulp.task('styles', function () {
     return  gulp.src('./app/assets/styles/styles.css')               // 1. it takes the gulp.src(the original CSS file)
-        .pipe(postcss([cssImport,mixins,  cssVars, nested, autoPreFixer]))   // 2. it takes it through the postCSS pipe which is an array of all the plugins
+        .pipe(postcss([cssImport,mixins,  cssVars, nested,hexrgba, autoPreFixer]))   // 2. it takes it through the postCSS pipe which is an array of all the plugins
         .on('error', function(errorInfo){
             console.log(errorInfo.toString());
             this.emit('end');
